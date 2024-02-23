c4fc404f4b8e0158230863064ac35d97ee9e9a47
Nikita Viira <aanikita62@gmail.com>
2024-02-23 11:08:38 +0200

    add initial scss files

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,7 +1,7 @@
-import './assets/main.css'
+import './assets/global.scss'
 
-import {createApp} from 'vue'
-import {createPinia} from 'pinia'
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
