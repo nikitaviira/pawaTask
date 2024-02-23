effcaae221625cb7926ece67bf9ef59cbaf6a43b
Nikita Viira <aanikita62@gmail.com>
2024-02-23 19:45:49 +0200

    lint

--- frontend/src/components/auth/AuthForm.vue
+++ frontend/src/components/auth/AuthForm.vue
@@ -15,33 +15,33 @@
 </script>
 
 <style scoped lang="scss">
-@import "@/assets/variables";
+  @import "@/assets/variables";
 
-.form {
-  margin-top: 20px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  height: 100%;
+  .form {
+    margin-top: 20px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
 
-  @media screen and (max-width: 600px) {
-    height: auto;
+    @media screen and (max-width: 600px) {
+      height: auto;
+    }
   }
-}
 
-.form-button {
-  margin-top: 30px;
-  padding: 15px 60px;
-  background: $main-color;
-  color: white;
-  font-weight: bold;
-  text-transform: uppercase;
-  border-radius: 15px;
-  cursor: pointer;
-
-  &:hover {
+  .form-button {
+    margin-top: 30px;
+    padding: 15px 60px;
     background: $main-color;
+    color: white;
+    font-weight: bold;
+    text-transform: uppercase;
+    border-radius: 15px;
+    cursor: pointer;
+
+    &:hover {
+      background: $main-color;
+    }
   }
-}
 </style>
