c1ba2548421444b19f28bb02503df3a1730a5a38
Nikita Viira <aanikita62@gmail.com>
2024-02-23 11:45:51 +0200

    add router auth checks

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,8 +1,40 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router';
+import Auth from '@/views/Auth.vue';
+import Tasks from '@/views/Tasks.vue';
+import PageNotFound from '@/views/PageNotFound.vue';
+import { useUserStore } from '@/stores/userStore';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: []
+  routes: [
+    {
+      path: '/',
+      name: 'tasks',
+      component: Tasks,
+      meta: { authRequired: true }
+    },
+    {
+      path: '/auth',
+      name: 'auth',
+      component: Auth,
+      meta: { disallowAuthed: true }
+    },
+    {
+      path: '/404',
+      component: PageNotFound
+    },
+    {
+      path: '/:pathMatch(.*)*',
+      redirect: '/404'
+    }
+  ]
 });
 
-export default router
+router.beforeEach((to, _, next) => {
+  const store = useUserStore();
+  if (to.meta.authRequired && !store.isLoggedIn) next({ name: 'auth' });
+  else if (to.meta.disallowAuthed && store.isLoggedIn) next({ name: 'tasks' });
+  else next();
+});
+
+export default router;
