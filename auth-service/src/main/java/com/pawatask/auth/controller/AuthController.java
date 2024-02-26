a83c491a5cc1dafff9204c2c7190a239d4e4a92c
Nikita Viira <aanikita62@gmail.com>
2024-02-26 13:53:27 +0200

    add missing @RestController

--- auth-service/src/main/java/com/pawatask/auth/controller/AuthController.java
+++ auth-service/src/main/java/com/pawatask/auth/controller/AuthController.java
@@ -7,7 +7,9 @@ import com.pawatask.auth.user.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @RequiredArgsConstructor
 public class AuthController extends BaseController {
   private final UserService userService;
