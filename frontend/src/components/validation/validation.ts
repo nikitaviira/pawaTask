b719df5f32575db4b34277b63fa004d029231c4b
Nikita Viira <aanikita62@gmail.com>
2024-02-28 19:19:33 +0200

    verify that password is shorter than 40 symbols on frontend

--- frontend/src/components/validation/validation.ts
+++ frontend/src/components/validation/validation.ts
@@ -2,7 +2,8 @@ export function passwordValidation(val: string): boolean {
   const hasDigit = /\d/.test(val);
   const hasUppercase = /[A-Z]/.test(val);
   const isMinimumLength = val.length >= 8;
-  return hasDigit && hasUppercase && isMinimumLength;
+  const isShorterThanMaxLength = val.length <= 40;
+  return hasDigit && hasUppercase && isMinimumLength && isShorterThanMaxLength;
 }
 
 export function emailValidation(val: string): boolean {
