3cd11d4000aff777acf54a75e9ee6cee75a7b447
Nikita Viira <aanikita62@gmail.com>
2024-02-29 18:35:00 +0200

    show default error in case of 500

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -6,6 +6,11 @@ import { toast } from 'vue3-toastify';
 
 const API_GATEWAY_URL = import.meta.env.VITE_API_GATEWAY_URL;
 
+interface ErrorDto {
+  message: string
+  fields: Record<string, string>
+}
+
 export default (): AxiosInstance => {
   const store = useUserStore();
   const instance = Axios.create({
@@ -15,18 +20,27 @@ export default (): AxiosInstance => {
   instance.defaults.headers.common['Access-Control-Allow-Origin'] = API_GATEWAY_URL;
 
   instance.interceptors.response.use((x) => x, async(error) => {
-    if (error.response && error.response.status === 401) {
-      store.logout();
-      await router.push('/auth');
+    if (error.response && error.response.status < 500) {
+      const { status, data }: { status: number, data: ErrorDto } = error.response;
+      switch (status) {
+        case 401:
+          store.logout();
+          await router.push('/auth');
+          break;
+        case 400:
+          toast.error(data.message);
+          break;
+      }
     } else {
-      toast.error(error.message);
-      throw error;
+      toast.error('Something went wrong!');
     }
+
+    throw error;
   });
 
   instance.interceptors.request.use(function(config) {
     if (store.isLoggedIn) {
-      config.headers.Authorization = `Bearer ${store.token}`;
+      config.headers.Authorization = `Bearer ${store.jwtToken}`;
     }
     return config;
   });
