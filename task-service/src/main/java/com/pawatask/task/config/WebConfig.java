6f6dcb76ac652ee3001016151c9d516f423a37a9
Nikita Viira <aanikita62@gmail.com>
2024-03-01 11:05:06 +0200

    add objectmapper config to handle localdates in json

--- task-service/src/main/java/com/pawatask/task/config/WebConfig.java
+++ task-service/src/main/java/com/pawatask/task/config/WebConfig.java
@@ -1,5 +1,7 @@
 package com.pawatask.task.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,6 +11,8 @@ import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
+
+import static com.fasterxml.jackson.databind.SerializationFeature.WRITE_DATES_AS_TIMESTAMPS;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -36,5 +40,13 @@ public class WebConfig implements WebMvcConfigurer {
     var bean = new LocalValidatorFactoryBean();
     bean.setValidationMessageSource(messageSource());
     return bean;
+  }
+
+  @Bean
+  public ObjectMapper objectMapper() {
+    var mapper = new ObjectMapper();
+    mapper.registerModule(new JavaTimeModule());
+    mapper.configure(WRITE_DATES_AS_TIMESTAMPS, false);
+    return mapper;
   }
 }
