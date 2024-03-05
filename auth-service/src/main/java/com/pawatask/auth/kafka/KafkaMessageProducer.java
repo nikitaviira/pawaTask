d0a7fa25912629fe6345e6cab9c13c9704dffa76
Nikita Viira <aanikita62@gmail.com>
2024-03-05 14:42:49 +0200

    add logs for kafka producer

--- auth-service/src/main/java/com/pawatask/auth/kafka/KafkaMessageProducer.java
+++ auth-service/src/main/java/com/pawatask/auth/kafka/KafkaMessageProducer.java
@@ -3,15 +3,23 @@ package com.pawatask.auth.kafka;
 import com.pawatask.kafka.KafkaJsonMessage;
 import com.pawatask.kafka.KafkaTopics;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class KafkaMessageProducer {
     private final KafkaTemplate<String, Object> kafkaTemplate;
 
     public void sendMessage(KafkaTopics topic, KafkaJsonMessage message) {
-        kafkaTemplate.send(topic.label, message);
+        kafkaTemplate.send(topic.label, message).whenComplete((result, throwable) -> {
+            if (throwable != null) {
+                log.error("Unable to send message=[" + message + "] due to : " + throwable.getMessage());
+            } else {
+                log.info("Sent message=[" + message + "] with offset=[" + result.getRecordMetadata().offset() + "]");
+            }
+        });
     }
 }
