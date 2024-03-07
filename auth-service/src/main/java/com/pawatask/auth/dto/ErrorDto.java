e4b54cb513b0f2e38172b824923266c1820d28f0
Nikita Viira <aanikita62@gmail.com>
2024-03-07 21:40:22 +0200

    one field may have multiple validation errors

--- auth-service/src/main/java/com/pawatask/auth/dto/ErrorDto.java
+++ auth-service/src/main/java/com/pawatask/auth/dto/ErrorDto.java
@@ -4,13 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @NoArgsConstructor
 @AllArgsConstructor
 public class ErrorDto {
   public String message;
-  public Map<String, String> fields = new HashMap<>();
+  public Map<String, List<String>> fields = new HashMap<>();
 
   public ErrorDto(String message) {
     this.message = message;
--- auth-service/src/main/java/com/pawatask/auth/exception/GlobalExceptionHandler.java
+++ auth-service/src/main/java/com/pawatask/auth/exception/GlobalExceptionHandler.java
@@ -5,18 +5,17 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 
-import static java.util.Map.entry;
-import static java.util.Objects.requireNonNull;
-import static java.util.stream.Collectors.toMap;
+import static java.util.stream.Collectors.*;
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 import static org.springframework.http.ResponseEntity.status;
@@ -37,12 +36,11 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
   @Override
   protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
-    Map<String, String> errors = ex
+    Map<String, List<String>> errors = ex
         .getBindingResult()
         .getFieldErrors()
         .stream()
-        .map(f -> entry(f.getField(), requireNonNull(f.getDefaultMessage())))
-        .collect(toMap(Entry::getKey, Entry::getValue));
+        .collect(groupingBy(FieldError::getField, mapping(FieldError::getDefaultMessage, toList())));
     return status(BAD_REQUEST).body(new ErrorDto("Validation failed", errors));
   }
 }
