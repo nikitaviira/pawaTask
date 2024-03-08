e4cd8ee35549a72da2252d4bb41d2803671168ca
Nikita Viira <aanikita62@gmail.com>
2024-03-08 13:36:23 +0200

    don't instantiate kafka producer in tests, add email topic creation

--- auth-service/src/main/java/com/pawatask/auth/kafka/KafkaProducerConfig.java
+++ auth-service/src/main/java/com/pawatask/auth/kafka/KafkaProducerConfig.java
@@ -7,6 +7,7 @@ import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.kafka.config.TopicBuilder;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaAdmin;
@@ -17,11 +18,13 @@ import org.springframework.kafka.support.serializer.JsonSerializer;
 import java.util.HashMap;
 import java.util.Map;
 
+import static com.pawatask.kafka.KafkaTopics.Names.EMAIL;
 import static com.pawatask.kafka.KafkaTopics.Names.USER;
 import static org.apache.kafka.clients.producer.ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG;
 import static org.apache.kafka.clients.producer.ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG;
 
 @Configuration
+@Profile("!integration")
 public class KafkaProducerConfig {
     @Value("${kafka.bootstrap.servers}")
     private String kafkaBootstrapServers;
@@ -50,5 +53,10 @@ public class KafkaProducerConfig {
     @Bean
     public NewTopic userTopic() {
         return TopicBuilder.name(USER).partitions(6).build();
+    }
+
+    @Bean
+    public NewTopic emailTopic() {
+        return TopicBuilder.name(EMAIL).partitions(6).build();
     }
 }
