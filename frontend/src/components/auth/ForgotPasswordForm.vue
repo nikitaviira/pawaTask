188f38e9bbcf90cbad9ed8a269865f61c95bb591
Nikita Viira <aanikita62@gmail.com>
2024-03-08 14:33:12 +0200

    add component for entering email to receive email to reset password

---
+++ frontend/src/components/auth/ForgotPasswordForm.vue
@@ -0,0 +1,55 @@
+<template>
+  <FormLayout
+    :error-text="errorText"
+    :success-text="successText"
+    :disable-submit-btn="disableSubmitBtn"
+    title="Password reset"
+    @submit="submitForm"
+  >
+    <InputWrapper
+      v-model.trim="passwordResetForm.email"
+      type="input"
+      label="Your account's email"
+      placeholder="Enter your email"
+      :validator="$v.email"
+    />
+  </FormLayout>
+</template>
+
+<script setup lang="ts">
+  import FormLayout from '@/components/auth/FormLayout.vue';
+  import InputWrapper from '@/components/auth/InputWrapper.vue';
+  import { ref } from 'vue';
+  import useVuelidate from '@vuelidate/core';
+  import { required } from '@vuelidate/validators';
+  import type { ErrorDto } from '@/api/client';
+  import passwordResetController, { type PasswordResetDto } from '@/api/controllers/passwordResetController';
+
+  const emit = defineEmits<{ (e: 'open-tab', tab: number): void }>();
+
+  const errorText = ref('');
+  const successText = ref('');
+  const disableSubmitBtn = ref(false);
+  const passwordResetForm = ref<PasswordResetDto>({ email: '' });
+  const $v = useVuelidate({ email: { required } }, passwordResetForm);
+
+  async function submitForm() {
+    await $v.value.$validate().then((result) => {
+      if (result) initPasswordReset();
+    });
+  }
+
+  async function initPasswordReset() {
+    disableSubmitBtn.value = true;
+    try {
+      await passwordResetController.initPasswordReset(passwordResetForm.value);
+      errorText.value = '';
+      successText.value = 'Instructions have been sent to your email';
+      setTimeout(() => emit('open-tab', 0), 2000);
+    } catch (error: any) {
+      const { data }: { data: ErrorDto } = error.response;
+      errorText.value = data?.message;
+      disableSubmitBtn.value = false;
+    }
+  }
+</script>
