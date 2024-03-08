1d60acaa40416b60b59a7241459cfd98a82a3c7a
Nikita Viira <aanikita62@gmail.com>
2024-03-08 15:01:19 +0200

    proper component name, show error alert in case email already exists

--- frontend/src/components/auth/RegisterTab.vue
+++ frontend/src/components/auth/RegisterTab.vue
@@ -1,11 +1,14 @@
 <template>
-  <AuthForm @submit="submitForm">
+  <AuthForm
+    :error-text="errorText"
+    @submit="submitForm"
+  >
     <InputWrapper
       v-model.trim="registerForm.email"
       type="input"
       label="Email"
       :validator="$v.email"
-      placeholder="Email"
+      placeholder="Enter your email"
     />
 
     <InputWrapper
@@ -13,14 +16,14 @@
       type="input"
       label="Username"
       :validator="$v.userName"
-      placeholder="Username"
+      placeholder="Enter your username"
     />
 
     <InputWrapper
       v-model.trim="registerForm.password"
       type="input-password"
       label="Password"
-      placeholder="Password"
+      placeholder="Enter your password"
       :validator="$v.password"
     />
 
@@ -28,7 +31,7 @@
       v-model.trim="registerForm.repeatedPassword"
       type="input-password"
       label="Repeat password"
-      placeholder="Repeat password"
+      placeholder="Enter your password again"
       :validator="$v.repeatedPassword"
     />
   </AuthForm>
@@ -41,9 +44,10 @@
   import useVuelidate from '@vuelidate/core';
   import { useUserStore } from '@/stores/userStore';
   import { useRouter } from 'vue-router';
-  import AuthForm from '@/components/auth/AuthForm.vue';
-  import InputWrapper from '@/components/auth/InputWrapper.vue';
+  import AuthForm from '@/components/auth/FormLayout.vue';
+  import InputWrapper from '@/components/generic/InputWrapper.vue';
   import { emailValid, passwordValid, repeatedPasswordValid } from '@/components/validation/validation';
+  import type { ErrorDto } from '@/api/client';
 
   const router = useRouter();
   const userStore = useUserStore();
@@ -52,6 +56,7 @@
     repeatedPassword: string;
   }
 
+  const errorText = ref('');
   const registerForm = ref<RegisterForm>({
     email: '',
     password: '',
@@ -59,7 +64,7 @@
     userName: ''
   });
 
-  const validationRules = {
+  const $v = useVuelidate({
     email: {
       required,
       emailValid,
@@ -79,21 +84,26 @@
       required,
       maxLength: maxLength(30)
     }
-  };
-
-  const $v = useVuelidate(validationRules, registerForm);
+  }, registerForm);
 
   async function submitForm() {
-    await $v.value.$validate().then(async(result) => {
-      if (result) {
-        await userStore.register({
-          email: registerForm.value.email,
-          password: registerForm.value.password,
-          userName: registerForm.value.userName
-        });
-        await toHomePage();
-      }
+    await $v.value.$validate().then((result) => {
+      if (result) register();
     });
+  }
+
+  async function register() {
+    try {
+      await userStore.register({
+        email: registerForm.value.email,
+        password: registerForm.value.password,
+        userName: registerForm.value.userName
+      });
+      await toHomePage();
+    } catch (error: any) {
+      const { data }: { data: ErrorDto } = error.response;
+      errorText.value = data?.message;
+    }
   }
 
   async function toHomePage() {
