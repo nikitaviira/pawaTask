a201dad85b6a2302cfdcc9717522941ad89c98e0
Nikita Viira <aanikita62@gmail.com>
2024-03-08 13:57:02 +0200

    allow sendgrid hook through gateway

--- gateway/src/main/java/com/pawatask/gateway/config/RouteConfig.java
+++ gateway/src/main/java/com/pawatask/gateway/config/RouteConfig.java
@@ -6,6 +6,8 @@ import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import static org.springframework.http.HttpMethod.POST;
+
 @Configuration
 @RequiredArgsConstructor
 public class RouteConfig {
@@ -17,6 +19,8 @@ public class RouteConfig {
     return builder.routes()
         .route("auth-service", r -> r.path("/api/auth/**")
             .uri(serviceUrls.getAuthService()))
+        .route("email-service", r -> r.method(POST).and().path("/sendgrid/processed-event")
+            .uri(serviceUrls.getEmailService()))
         .route("task-service", r -> r.path("/api/task/**")
             .filters(f -> f.filter(authFilter))
             .uri(serviceUrls.getTaskService()))
--- gateway/src/main/java/com/pawatask/gateway/config/ServiceUrls.java
+++ gateway/src/main/java/com/pawatask/gateway/config/ServiceUrls.java
@@ -12,4 +12,5 @@ import org.springframework.stereotype.Component;
 public class ServiceUrls {
     private String authService;
     private String taskService;
+    private String emailService;
 }
