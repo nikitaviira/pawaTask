93fb5c951ae5e8418323c4b78a2d3fdc15e8e1d6
Nikita Viira <aanikita62@gmail.com>
2024-03-10 15:39:46 +0200

    move all kafka producer configuration to application.properties, enable kafka transactions

--- auth-service/src/main/java/com/pawatask/auth/kafka/KafkaProducerConfig.java
+++ auth-service/src/main/java/com/pawatask/auth/kafka/KafkaProducerConfig.java
@@ -1,62 +1,20 @@
 package com.pawatask.auth.kafka;
 
-import org.apache.kafka.clients.admin.AdminClientConfig;
-import org.apache.kafka.clients.admin.NewTopic;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.common.serialization.StringSerializer;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.kafka.config.TopicBuilder;
-import org.springframework.kafka.core.DefaultKafkaProducerFactory;
-import org.springframework.kafka.core.KafkaAdmin;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.core.ProducerFactory;
-import org.springframework.kafka.support.serializer.JsonSerializer;
-
-import java.util.HashMap;
-import java.util.Map;
+import org.springframework.kafka.core.KafkaAdmin.NewTopics;
 
 import static com.pawatask.kafka.KafkaTopics.Names.EMAIL;
 import static com.pawatask.kafka.KafkaTopics.Names.USER;
-import static org.apache.kafka.clients.producer.ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG;
-import static org.apache.kafka.clients.producer.ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG;
 
 @Configuration
-@Profile("!integration")
 public class KafkaProducerConfig {
-    @Value("${kafka.bootstrap.servers}")
-    private String kafkaBootstrapServers;
-
     @Bean
-    public ProducerFactory<String, Object> producerFactory() {
-        Map<String, Object> configs = new HashMap<>();
-        configs.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaBootstrapServers);
-        configs.put(KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        configs.put(VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
-        return new DefaultKafkaProducerFactory<>(configs);
-    }
-
-    @Bean
-    public KafkaTemplate<String, Object> kafkaTemplate() {
-        return new KafkaTemplate<>(producerFactory());
-    }
-
-    @Bean
-    public KafkaAdmin kafkaAdmin() {
-        Map<String, Object> configs = new HashMap<>();
-        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaBootstrapServers);
-        return new KafkaAdmin(configs);
-    }
-
-    @Bean
-    public NewTopic userTopic() {
-        return TopicBuilder.name(USER).partitions(6).build();
-    }
-
-    @Bean
-    public NewTopic emailTopic() {
-        return TopicBuilder.name(EMAIL).partitions(6).build();
+    public NewTopics topics() {
+        return new NewTopics(
+            TopicBuilder.name(USER).replicas(1).partitions(6).build(),
+            TopicBuilder.name(EMAIL).replicas(1).partitions(6).build()
+        );
     }
 }
